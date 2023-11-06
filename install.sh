01bae125c346aa4dba63128e8837a91a15b1964b
zeki <nguyenhatrung411@gmail.com>
2023-11-06 12:04:25 +0000

    update

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-docker create -it \
+docker run -itd \
     --name ros_gnss \
     -e DISPLAY=host.docker.internal:0.0 \
     --net=host \
@@ -6,20 +6,18 @@ docker create -it \
     -v /dev:/dev \
     serene4utobot/gpsrtk:galactic-ubuntu20.04 
 
-docker start ros_gnss
-docker update -itd ros_gnss
 docker exec -it ros_gnss bash -c "cd ~ && apt remove python3-pip -y \
-    # && apt install wget -y \
-    # && wget https://bootstrap.pypa.io/get-pip.py \
-    # && python3 get-pip.py \
-    # && pip install pyopenssl --upgrade \
-    # && mkdir -p ros_gnss_ws/src \
-    # && cd ros_gnss_ws/src \
-    # && git clone https://github.com/serene4uto/rtcm_provider_ros.git \
-    # && pip3 install -r rtcm_provider_ros/requirements.txt \
-    # && git clone https://github.com/serene4uto/ublox_gnss_ros.git \
-    # && pip3 install -r ublox_gnss_ros/requirements.txt \
-    # && cd .. \
-    # && colcon build \
-    # && exit"
+    && apt install wget -y \
+    && wget https://bootstrap.pypa.io/get-pip.py \
+    && python3 get-pip.py \
+    && pip install pyopenssl --upgrade \
+    && mkdir -p ros_gnss_ws/src \
+    && cd ros_gnss_ws/src \
+    && git clone https://github.com/serene4uto/rtcm_provider_ros.git \
+    && pip3 install -r rtcm_provider_ros/requirements.txt \
+    && git clone https://github.com/serene4uto/ublox_gnss_ros.git \
+    && pip3 install -r ublox_gnss_ros/requirements.txt \
+    && cd .. \
+    && colcon build \
+    && exit"
 
