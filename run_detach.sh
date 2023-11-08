fa52815215c161eee82519aeaf712bdef392f6c1
zeki <nguyenhatrung411@gmail.com>
2023-11-08 04:26:19 +0000

    up

--- run_detach.sh
+++ run_detach.sh
@@ -1,3 +1,3 @@
 docker start ros_gnss
-docker exec  ros_gnss bash -c "cd ~/ros_gnss_ws && source ./install/setup.bash \
+docker exec ros_gnss bash -c "cd ~/ros_gnss_ws && source ./install/setup.bash \
         && ros2 launch ublox_gnss gnssrtk.launch.py"
