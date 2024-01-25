edb261e60c90d9b57d63d318a43e18b072935bf2
zeki <nguyenhatrung411@gmail.com>
2024-01-25 12:32:20 +0000

    up

--- run_mapviz.sh
+++ run_mapviz.sh
@@ -1,3 +1,5 @@
+xhost +local:docker
+export DISPLAY=:0
 docker start ros_gnss
 docker exec -it ros_gnss bash -c "cd ~/ros_gnss_ws && source ./install/setup.bash \
-        && ros2 launch ublox_gnss_ros mapviz.launch.py"
+        && export DISPLAY=:0 && ros2 launch ublox_gnss mapviz.launch.py"
